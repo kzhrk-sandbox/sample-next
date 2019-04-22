1edbbf4a671255553e2d3d19122133d0bbff0396
kzhrk <info@kzhrk.com>
2019-04-23 01:43:26 +0900

    Add express

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+
+    server
+      .get('/work/:id', (req, res) => {
+        app.render(req, res, '/work', {id: req.params.id })
+      })
+      .get('*', (req, res) => {
+        return handle(req, res)
+      })
+      .listen(3000, err => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+      })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
